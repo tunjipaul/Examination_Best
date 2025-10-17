3f563512e053c83aebb508ab2716dd0831d9a020
TunjiPaul <ogorpaul877@gmail.com>
2025-10-17 15:30:17 +0100

    updated

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
-function showDevelopers(){
-     document.getElementById("developers-block").style.display = "flex";
-     document.getElementById("dev-btn").style.display = "none";
-     document.getElementById("eng-btn").style.display = "none";
-   }
-   function showEngineers(){
-    document.getElementById("engineers-block").style.display = "flex";
-    document.getElementById("eng-btn").style.display = "none";
-    document.getElementById("dev-btn").style.display = "none"
-   }
+function showDevelopers() {
+  document.getElementById("developers-block").style.display = "flex";
+  document.getElementById("dev-btn").style.display = "none";
+  document.getElementById("eng-btn").style.display = "none";
+}
+function showEngineers() {
+  document.getElementById("engineers-block").style.display = "flex";
+  document.getElementById("eng-btn").style.display = "none";
+  document.getElementById("dev-btn").style.display = "none";
+}
+function backDevelopers(){
+  document.getElementById("developers-block").style.display = "none";
+  document.getElementById("dev-btn").style.display = "flex";
+  document.getElementById("eng-btn").style.display = "flex";
+}
+
+function backEngineers(){
+  document.getElementById("engineers-block").style.display = "none";
+  document.getElementById("eng-btn").style.display = "flex";
+  document.getElementById("dev-btn").style.display = "flex";
+}
