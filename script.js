6082fb689ca9d24c29d606367d739d7b1d022c27
TunjiPaul <ogorpaul877@gmail.com>
2025-10-17 15:14:45 +0100

    updated

---
+++ script.js
@@ -0,0 +1,10 @@
+function showDevelopers(){
+     document.getElementById("developers-block").style.display = "flex";
+     document.getElementById("dev-btn").style.display = "none";
+     document.getElementById("eng-btn").style.display = "none";
+   }
+   function showEngineers(){
+    document.getElementById("engineers-block").style.display = "flex";
+    document.getElementById("eng-btn").style.display = "none";
+    document.getElementById("dev-btn").style.display = "none"
+   }
